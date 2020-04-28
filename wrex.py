eba6d70915638f2b239f60332e5ae127191fcda7
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2020-04-28 16:56:53 +0300

    renamed main program script from 'run.py' to 'wrex.py'

--- wrex.py
+++ wrex.py
@@ -3,16 +3,41 @@ from extraction.content_parser import ContentParser
 from excel.excel_doc_generator import ExcelGenerator
 
 import json
+import sys
 
 content_reader = ContentReader()
 # "sample_mwbs/mwb_E_201909.epub", "sample_mwbs/mwb_E_202001.epub", "sample_mwbs/mwb_E_202004.epub"
 # "sample_mwbs/mwb_TI_201904.epub"
 # "sample_mwbs/mwb_AM_201904.epub", "sample_mwbs/mwb_AM_201905.epub"
 # "sample_mwbs/mwb_F_201912.epub"
-file_paths = ["sample_mwbs/mwb_E_201909.epub", "sample_mwbs/mwb_E_202001.epub", "sample_mwbs/mwb_E_202004.epub"]
+
+# parse options and arguments from sys.argv
+
+options = []
+arguments = []
+
+for string in sys.argv[1:]:
+    if string[0:2] == '--':
+        options.append(string)
+    else:
+        arguments.append(string)
+
+if '--help' in options:
+    print('WREX-PY: The Python version of WREX! This is such a cool application!')
+    sys.exit()
+elif '--version' in options:
+    print('v0.1 [build 1053]')
+    sys.exit()
+
+if len(arguments) == 0:
+    print('No publications passed. Exiting...')
+    sys.exit()
+
+file_paths = arguments
+
 epub_files = []
 
-for file_path in file_paths:
+for file_path in sorted(file_paths):
     epub_files.append(open(file_path, mode='rb'))
 
 pub_extracts = content_reader.get_publication_extracts(epub_files)
