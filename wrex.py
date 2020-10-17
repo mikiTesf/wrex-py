0d76145ef3024dd25384dfc5c0b6f03b1d5a855a
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2020-10-17 16:24:16 +0300

    moved interface functions into a class; standardized cli with argparse

--- wrex.py
+++ wrex.py
@@ -1,89 +1,79 @@
-from extraction.content_reader import ContentReader
+import argparse
+import json
+import sys
+import re
+from os.path import join, basename
+
 from extraction.content_parser import ContentParser
+from extraction.content_reader import ContentReader
 from excel.excel_doc_generator import ExcelGenerator
 
-import json, sys, collections
-from os import sep
+
+class WREX:
+
+    def __init__(self):
+        self._LANGUAGE_FOLDER = 'language'
+        self._LANG_CODE_PAIR = None
+
+        try:
+            lang_resolver_path = join(self._LANGUAGE_FOLDER, 'lang_code.json')
+            with open(lang_resolver_path, mode='r') as lang_resolver:
+                self._LANG_CODE_PAIR = json.load(lang_resolver)
+        except FileNotFoundError:
+            sys.exit('Could not load language resolver. Exiting...')
+
+    def get_lang_pack(self, lang_code: str = 'AM'):
+        lang_pack_name = self._LANG_CODE_PAIR[lang_code] + '.json'
+        lang_pack = open(join(self._LANGUAGE_FOLDER, lang_pack_name), mode='r')
+        return json.load(lang_pack)
+
+    def read_pubs_and_generate_excel_doc(self, file_args: list):
+        lang_pub_pair = self.get_lang_pub_pair(file_args)
+        content_reader = ContentReader()
+        content_parser = ContentParser()
+
+        for lang_key in lang_pub_pair:
+            language_pack = self.get_lang_pack(lang_code=lang_key)
+            # read contents of the publications of the given language key (lang_key)
+            pub_extracts = content_reader.get_publication_extracts(lang_pub_pair[lang_key])
+            # parse contents
+            content_parser.all_publication_extracts = pub_extracts
+            content_parser.filter_for_minute = language_pack['filter_for_minute']
+            pub_extracts = content_parser.build_meeting_objects()
+            # generate Excel document
+            excel_generator = ExcelGenerator(pub_extracts, language_pack)
+            excel_generator.create_excel_doc()
+
+    @staticmethod
+    def get_lang_pub_pair(file_args: list):
+        lang_pub_pair = {}
+
+        for pub_file in file_args:
+            match = re.match('^mwb_(\\w+)_\\d+\\.epub$', basename(pub_file.name), re.IGNORECASE)
+
+            if lang_pub_pair.get(match.group(1)) is None:
+                lang_pub_pair[match.group(1)] = [pub_file]
+            else:
+                lang_pub_pair[match.group(1)].append(pub_file)
+        return lang_pub_pair
 
 
-content_reader = ContentReader()
-VERSION = 'v0.1'
-LANGUAGE_FOLDER = 'language' + sep
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        prog='wrex-py',
+        description="wrex-py (from the original wrex written in Java) extracts the presentations in a Meeting Workbook\
+                    and prepares an Excel document making assignments easy for the responsible Elder or Ministerial\
+                    Servant. It is mandatory that all files passed to wrex-py be in the EPUB format.",
+        allow_abbrev=False)
 
-try:
-    with open('HELP', 'r') as help_file:
-        HELP = help_file.read()
-except FileNotFoundError:
-    HELP = 'Help file is missing. Please read the documentation at https://www.github.com/mikiTesf/wrex-py'
+    arg_parser.version = 'version 0.1'
+    arg_parser.add_argument('path', action='store',
+                            help='path to a meeting workbook file(s)', nargs='+')
+    arg_parser.add_argument('-v', '--version', action='version')
 
-try:
-    lang_resolver_path = LANGUAGE_FOLDER + 'lang_code.json'
-    with open(lang_resolver_path, mode='r') as lang_resolver:
-        LANG_CODE_PAIR = json.load(lang_resolver)
-except FileNotFoundError:
-    print(lang_resolver_path, 'missing. Exiting...')
-    sys.exit()
+    parsed_args = arg_parser.parse_args()
 
-
-def get_lang_code(pub_name):
-    last_file_separator_index = pub_name.find(sep)
-    pub_name = pub_name[last_file_separator_index + 1:]
-    first_underscore_index = pub_name.find('_')
-    second_underscore_index = pub_name.find('_', first_underscore_index + 1)
-    return pub_name[first_underscore_index + 1:second_underscore_index]
-
-
-def get_lang_pack(lang_code='E'):
-    lang_pack_name = LANG_CODE_PAIR[lang_code] + '.json'
-    lang_pack = open(LANGUAGE_FOLDER + lang_pack_name, mode='r')
-    return json.load(lang_pack)
-
-
-options = []
-file_args = []
-
-raw_arguments = collections.deque(sys.argv[1:])
-
-if len(raw_arguments) == 0:
-    print(HELP)
-    sys.exit()
-
-while len(raw_arguments) != 0:
-    arg = raw_arguments.popleft()
-
-    if len(file_args) == 0:
-        if arg in ['-h', '--help']:
-            print(HELP)
-            sys.exit()
-        if arg in ['-v', '--version']:
-            print(VERSION)
-            sys.exit()
-
-    try:
-        file_args.append(open(arg, mode='rb'))
-    except (FileNotFoundError, IsADirectoryError):
-        print(HELP)
-        sys.exit()
-
-
-lang_pub_pair = {}
-
-for pub_file in file_args:
-    lang_code = get_lang_code(pub_file.name)
-
-    if lang_pub_pair.get(lang_code) is None:
-        lang_pub_pair[lang_code] = [pub_file]
-    else:
-        lang_pub_pair[lang_code].append(pub_file)
-
-content_parser = ContentParser()
-
-for lang_key in lang_pub_pair:
-    language_pack = get_lang_pack(lang_code=lang_key)
-    pub_extracts = content_reader.get_publication_extracts(lang_pub_pair[lang_key])
-    content_parser.entire_publication_extracts = pub_extracts
-    content_parser.filter_for_minute = language_pack['filter_for_minute']
-    pub_extracts = content_parser.build_meeting_objects()
-
-    excel_generator = ExcelGenerator(pub_extracts, language_pack)
-    excel_generator.create_excel_doc()
+    wrex = WREX()
+    wrex.read_pubs_and_generate_excel_doc(
+        [open(pub, 'rb') for pub in parsed_args.path]
+    )
